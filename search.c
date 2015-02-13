1e035e0ffb0725d494b473b46679501518fd6d25
Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
2015-02-13 03:24:17 -0200

    First commit

---
+++ search.c
@@ -0,0 +1,82 @@
+/*
+* Copyright (C) 2015
+*
+*
+* Written by Raphael S. Carvalho <raphael.scarv@gmail.com>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <errno.h>
+#include <assert.h>
+#include "vector.h"
+
+void search(const char *path, const char *name, vector *v, int max_depth) {
+    if (max_depth == 0) {
+        return;
+    }
+
+    DIR *dir = opendir(path);
+    assert(dir);
+    struct dirent *dir_entry;
+    char *next_path;
+
+    while ((dir_entry = readdir(dir)) != NULL) {
+        // skip "." and ".."
+        if (!strcmp(dir_entry->d_name, ".") || !strcmp(dir_entry->d_name, "..")) {
+            continue;
+        }
+
+        if (dir_entry->d_type == DT_DIR) {
+            next_path = malloc(strlen(path) + 1 + strlen(dir_entry->d_name) + 1);
+            assert(next_path);
+            sprintf(next_path, "%s/%s", path, dir_entry->d_name);
+            search(next_path, name, v, max_depth - 1);
+            free(next_path);
+        }
+        // find occurrence of substring name in string d_name.
+        if (name && !strstr(dir_entry->d_name, name)) {
+            continue;
+        }
+
+        (void) vector_insert(v, dir_entry);
+    }
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2 && argc != 3) {
+        fprintf(stderr, "%s <path> <string>\n", argv[0]);
+        return -1;
+    }
+    
+    vector v = vector_create(1.75f, sizeof(struct dirent));
+    search(argv[1], (argc == 2) ? NULL : argv[2], &v, 3);
+
+    int i;
+    for (i = i; i < vector_length(&v); i++) {
+        struct dirent *entry = vector_access(&v, i);
+        printf("%s\n", entry->d_name);
+    }
+    
+    printf("vector footprint: %d\n", vector_footprint(&v));
+
+    vector_destroy(&v);
+
+    return 0;
+}
---
+++ vector.h
@@ -0,0 +1,94 @@
+/*
+* Copyright (C) 2015
+*
+*
+* Written by Raphael S. Carvalho <raphael.scarv@gmail.com>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#ifndef _VECTOR_H_
+#define _VECTOR_H_
+
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct vector {
+    void *data;
+    float growth_factor;
+    int type_size;
+    int size;
+    int capacity;
+} vector;
+
+vector vector_create(float growth_factor, int type_size) {
+    static const int initial_capacity = 10;
+    vector new = {
+        .data = malloc(initial_capacity * type_size),
+        .growth_factor = growth_factor,
+        .type_size = type_size,
+        .size = 0,
+        .capacity = initial_capacity,
+    };
+    assert(new.data != NULL);
+    return new;
+}
+
+void vector_destroy(vector *v) {
+    free(v->data);
+    memset((void *) v, 0, sizeof(vector));
+}
+
+inline void *vector_access(vector *v, int i) {
+    assert(i >= 0 && i < v->capacity);
+    unsigned offset = i * v->type_size;
+    return (void *)((char *)v->data + offset);
+}
+
+inline int vector_length(vector *v) {
+    return v->size;
+}
+
+void *vector_insert(vector *v, void *object) {
+    assert(v->capacity > 0);
+    assert(v->size <= v->capacity);
+    if (v->size == v->capacity) {
+        // increase vector capacity by growth factor.
+        int new_capacity = (int) (v->capacity * v->growth_factor);
+        v->data = realloc(v->data, new_capacity * v->type_size);
+        assert(v->data != NULL);
+        v->capacity = new_capacity;
+    }    
+
+    int type_size = v->type_size;
+    unsigned offset = v->size * type_size;
+    void *dest = (void *)((char *)v->data + offset);
+    assert(offset + type_size <= v->capacity * type_size);
+    if (object) {
+        // copy object into vector
+        memcpy(dest, object, type_size);
+    }
+    v->size++;
+    return dest;
+}
+
+int vector_footprint(vector *v) {
+    return v->size * v->type_size;
+}
+
+#endif
+
+
